7d5b2d23b0e491d0dd5617b99af863e95a0437bf
Sourin Manna <sourinmanna31012002@gmail.com>
2023-09-15 12:13:59 +0530

    Cart Adapter Created

--- app/src/main/java/in/opmeals/Adapters/CartAdapter.kt
+++ app/src/main/java/in/opmeals/Adapters/CartAdapter.kt
@@ -1,5 +1,36 @@
 package `in`.opmeals.Adapters
 
-class CartAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import `in`.opmeals.databinding.CartItemBinding
+
+class CartAdapter(private val cartItems:MutableList<String>, private val cartItemsPrice:MutableList<String>, private val CartImage: MutableList<Int>): RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
+
+    private val itemQuantities = IntArray(cartItems.size){1}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        var binding = CartItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return CartViewHolder(binding)
+    }
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = cartItems.size
+
+    inner class CartViewHolder(private val binding: CartItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            binding.apply {
+
+                val quantity = itemQuantities[position]
+
+                cartFoodName.text = cartItems[position]
+                cartPrice.text = cartItemsPrice[position]
+                cartImage.setImageResource(CartImage[position])
+                cartItemQuantity.text = quantity.toString()
+            }
+        }
+
+    }
 
 }
